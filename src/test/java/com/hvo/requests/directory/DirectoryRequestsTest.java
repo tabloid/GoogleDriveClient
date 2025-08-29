695a476927e07e76356d218413012b1b53d1a020
Vsevolod H <sysmet@meta.ua>
2025-08-30 02:36:12 +0300

    add deleteDirectoryRequestTest, refactor

--- src/test/java/com/hvo/requests/directory/DirectoryRequestsTest.java
+++ src/test/java/com/hvo/requests/directory/DirectoryRequestsTest.java
@@ -4,13 +4,14 @@ package com.hvo.requests.directory;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class RequestsTest {
+public class DirectoryRequestsTest {
 
     private static final String accessToken = "someToken";
+    private static final String directoryName = "someDirectoryName";
+    private static final String directoryId = "someDirectoryId";
 
     @Test
     public void createDirectoryRequestTest() {
-        String directoryName = "someDirectoryName";
         CreateDirectoryRequest request = new CreateDirectoryRequest(accessToken, directoryName);
         String curl = request.getCurl();
         String expectedCurl = "curl https://www.googleapis.com/drive/v3/files --request POST --verbose  --header 'Authorization: Bearer someToken' --header 'Accept: application/json' --header 'Content-Type: application/json' --data '{\"name\":\"someDirectoryName\",\"mimeType\":\"application/vnd.google-apps.folder\",\"parent\":[\"root\"]}'";
@@ -18,8 +19,15 @@ public class RequestsTest {
     }
 
     @Test
+    public void deleteDirectoryRequestTest() {
+        DeleteDirectoryRequest request = new DeleteDirectoryRequest(accessToken, directoryId);
+        String curl = request.getCurl();
+        String expectedCurl = "curl https://www.googleapis.com/drive/v3/files/someDirectoryId --request DELETE --verbose  --header 'Authorization: Bearer someToken'";
+        Assert.assertEquals(expectedCurl, curl);
+    }
+
+    @Test
     public void getDirectoryRequestTest() {
-        String directoryId = "someDirectoryId";
         GetDirectoryRequest request = new GetDirectoryRequest(accessToken, directoryId);
         String curl = request.getCurl();
         String expectedCurl = "curl https://www.googleapis.com/drive/v3/files/someDirectoryId?fields=* --request GET --verbose  --header 'Authorization: Bearer someToken' --header 'Accept: application/json'";
