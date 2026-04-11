0ca6747f4e96a87b60ec6120a4876be2e6dee1f5
Vsevolod H <sysmet@meta.ua>
2026-04-11 14:04:20 +0300

    add upload fie url for the future usage

--- src/main/java/com/hvo/lib/constants/API.java
+++ src/main/java/com/hvo/lib/constants/API.java
@@ -3,4 +3,5 @@ package com.hvo.lib.constants;
 public class API {
     public static final String TOKEN = "https://oauth2.googleapis.com/token";
     public static final String FILES = "https://www.googleapis.com/drive/v3/files";
+    public static final String UPLOAD_FILE = "https://www.googleapis.com/upload/drive/v3/files";
 }
